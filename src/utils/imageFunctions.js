6f6e8dc558678a8a77297491add6dbbfff7c9bbb
sowjanya <sowji.gsetty@gmail.com>
2026-03-23 14:14:37 +0100

    Fix export statement for getImageURL function

--- src/utils/imageFunctions.js
+++ src/utils/imageFunctions.js
@@ -1,3 +1,3 @@
-export const getImageURL = imageParameter => { //totoro.jpg
+export const getImageURL = imageParameter => { 
     return new URL(`../assets/images/${imageParameter}`, import.meta.url).href
 }
